800a72aa8084bb7bc62050e619de53a765f13cfa
VaradSinghal <varadsinghal1526@gmail.com>
2025-12-26 16:19:11 +0530

    day001

---
+++ day-001/solution.py
@@ -0,0 +1,23 @@
+def solve():
+    t = int(input())
+    for _ in range(t):
+        a, b, c = map(int, input().split())
+        
+        d = abs(a - b)
+        n = 2 * d
+        
+        # Check validity
+        if n == 0 or max(a, b, c) > n:
+            print(-1)
+            continue
+        
+        half = n // 2
+        
+        # Find opposite of c
+        if c + half <= n:
+            print(c + half)
+        else:
+            print(c - half)
+
+# Run
+solve()
